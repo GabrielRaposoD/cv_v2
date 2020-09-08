ab984339039684a8d6ff53656c174ccf5dd01243
Gabriel Raposo <gabriel.robertor@gmail.com>
2020-09-08 20:39:52 -0300

    Finished styling the hero and the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 export function Header() {
   return (
-    <div className="fixed w-full border-b border-gray-900 border-opacity-75">
+    <div className="fixed z-10 w-full border-b border-gray-900 border-opacity-75">
       <div className="bg-gradient-to-l from-primary-500 to-primary-900 h-1"></div>
       <div className="flex flex-row justify-between px-20 py-6 mx-auto bg-transparent">
         <div className="flex flex-row items-center">
-          <img src="/images/fox.png" alt="" className="h-8 mr-2" />
-          <img src="/images/logo.png" alt="" className="h-12" />
+          <img src="/images/fox.png" alt="" className="h-8 mr-4" />
+          <img src="/images/logo.png" alt="" className="h-12 -mb-2" />
         </div>
-        <div className="flex flex-row items-center space-x-4 text-gray-100">
-          <button className="font-semibold">About</button>
-          <button className="font-semibold">Portfolio</button>
-          <button className="font-semibold">Services</button>
-          <button className="font-semibold">Contact</button>
+        <div className="flex flex-row items-center space-x-12 text-gray-200">
+          <button className="linker font-semibold">About</button>
+          <button className="linker font-semibold">Portfolio</button>
+          <button className="linker font-semibold">Services</button>
+          <button className="linker font-semibold">Contact</button>
         </div>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,38 @@
 export function Hero() {
   return (
     <section className="relative flex min-h-screen">
-      <div className="flex flex-col items-end justify-center w-full px-20 space-y-12">
-        <h1 className="text-6xl font-bold leading-tight text-left text-white">
-          PROGRESS SHOULD <br />{' '}
-          <span className="text-primary-500">NEVER </span>
-          BE KEPT <br /> WAITING
-        </h1>
-        <div className="border-primary-700 border-l-4 border-solid">oi</div>
-        <div className="flex flex-row space-x-10 text-3xl text-white">
-          <i className="fab fa-github-alt"></i>
-          <i className="fab fa-linkedin-in "></i>
-          <i className="fab fa-instagram"></i>
+      <div className="flex flex-col items-end justify-center w-full px-20">
+        <div className="space-y-12">
+          <h1 className=" text-6xl font-bold leading-tight text-left text-white">
+            PROGRESS SHOULD <br />{' '}
+            <span className="text-primary-500">NEVER </span>
+            BE KEPT <br /> WAITING
+          </h1>
+
+          <p className="border-primary-700 pl-4 text-lg font-semibold leading-loose text-gray-200 border-l-4 border-solid">
+            I'm <span className="text-primary-500">Gabriel Raposo, </span>a
+            Fullstack Developer and Freelancer. <br /> Let's work together!
+          </p>
+          <div className="flex flex-row space-x-10 text-3xl text-gray-200">
+            <a
+              href="https://github.com/GabrielRaposoD"
+              className="hover:text-primary-500 transition-all duration-500 ease-in-out"
+            >
+              <i className="fab fa-github-alt"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/gabrielrraposo/"
+              className="hover:text-primary-500 transition-all duration-500 ease-in-out"
+            >
+              <i className="fab fa-linkedin-in "></i>
+            </a>
+            <a
+              href="instagram.com/gabrielrraposo"
+              className="hover:text-primary-500 transition-all duration-500 ease-in-out"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
         </div>
       </div>
       <img
